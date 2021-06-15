e12b4ba65fc7f9130c394200f31c681977ac243f
Zijian-Wang <ryegogogo@gmail.com>
2021-06-15 19:15:10 +0800

    Update RotationRepeater0.1.jsx
    Attempt to resolve the "not deleting old keyframes" bug;
    Attempt to add support for vector properties.

--- RotationRepeater0.1.jsx
+++ RotationRepeater0.1.jsx
@@ -1,29 +1,24 @@
 (function(thisObj){
 
-    //createPannel();
-
     var panelGlobal = thisObj;
-
-    //function createPannel(){
-        
 
         /*
         Code for Import https://scriptui.joonas.me — (Triple click to select): 
-        {"activeId":14,"items":{"item-0":{"id":0,"type":"Dialog","parentId":false,"style":{"enabled":true,"varName":"RotationRepeaterWindow","windowType":"Palette","creationProps":{"su1PanelCoordinates":true,"maximizeButton":false,"minimizeButton":false,"independent":false,"closeButton":true,"borderless":false,"resizeable":true},"text":"Rotation Repeater","preferredSize":[300,0],"margins":16,"orientation":"column","spacing":10,"alignChildren":["fill","center"]}},"item-1":{"id":1,"type":"StaticText","parentId":5,"style":{"enabled":true,"varName":null,"creationProps":{"truncate":"none","multiline":false,"scrolling":false},"softWrap":false,"text":"Key 1: ","justify":"left","preferredSize":[0,0],"alignment":"fill","helpTip":null}},"item-2":{"id":2,"type":"EditText","parentId":5,"style":{"enabled":true,"varName":"key1Val","creationProps":{"noecho":false,"readonly":false,"multiline":false,"scrollable":false,"borderless":false,"enterKeySignalsOnChange":false},"softWrap":false,"text":"-5","justify":"left","preferredSize":[50,0],"alignment":"fill","helpTip":"第一关键帧数值"}},"item-3":{"id":3,"type":"Panel","parentId":0,"style":{"enabled":true,"varName":"RotationRepeate","creationProps":{"borderStyle":"etched","su1PanelCoordinates":true},"text":"Rotation Repeat （重复旋转关键帧）","preferredSize":[0,0],"margins":10,"orientation":"row","spacing":40,"alignChildren":["left","top"],"alignment":null}},"item-5":{"id":5,"type":"Group","parentId":3,"style":{"enabled":true,"varName":"key1","preferredSize":[0,0],"margins":0,"orientation":"row","spacing":10,"alignChildren":["left","center"],"alignment":null}},"item-10":{"id":10,"type":"Group","parentId":3,"style":{"enabled":true,"varName":"key2","preferredSize":[0,0],"margins":0,"orientation":"row","spacing":10,"alignChildren":["left","center"],"alignment":null}},"item-11":{"id":11,"type":"StaticText","parentId":10,"style":{"enabled":true,"varName":null,"creationProps":{"truncate":"none","multiline":false,"scrolling":false},"softWrap":false,"text":"Key 2: ","justify":"left","preferredSize":[0,0],"alignment":"fill","helpTip":null}},"item-12":{"id":12,"type":"EditText","parentId":10,"style":{"enabled":true,"varName":"key2Val","creationProps":{"noecho":false,"readonly":false,"multiline":false,"scrollable":false,"borderless":false,"enterKeySignalsOnChange":false},"softWrap":false,"text":"5","justify":"left","preferredSize":[50,0],"alignment":"fill","helpTip":"中间关键帧数值"}},"item-14":{"id":14,"type":"Button","parentId":0,"style":{"enabled":true,"varName":"applyBttn","text":"Apply | 应用","justify":"center","preferredSize":[0,0],"alignment":"center","helpTip":null}},"item-15":{"id":15,"type":"Group","parentId":3,"style":{"enabled":true,"varName":"freq","preferredSize":[0,0],"margins":0,"orientation":"row","spacing":10,"alignChildren":["center","center"],"alignment":null}},"item-16":{"id":16,"type":"StaticText","parentId":15,"style":{"enabled":true,"varName":null,"creationProps":{"truncate":"none","multiline":false,"scrolling":false},"softWrap":false,"text":"Frequency: ","justify":"left","preferredSize":[0,0],"alignment":"fill","helpTip":"频率（按秒数计算）"}},"item-17":{"id":17,"type":"EditText","parentId":15,"style":{"enabled":true,"varName":"freq","creationProps":{"noecho":false,"readonly":false,"multiline":false,"scrollable":false,"borderless":false,"enterKeySignalsOnChange":false},"softWrap":false,"text":"0.1","justify":"left","preferredSize":[50,0],"alignment":"fill","helpTip":null}},"item-18":{"id":18,"type":"Checkbox","parentId":3,"style":{"enabled":true,"varName":"holdFrame","text":"Hold Keyframe","preferredSize":[0,0],"alignment":"center","helpTip":null}}},"order":[0,3,5,1,2,10,11,12,15,16,17,18,14],"settings":{"importJSON":true,"indentSize":false,"cepExport":false,"includeCSSJS":true,"showDialog":false,"functionWrapper":false,"afterEffectsDockable":true,"itemReferenceList":"var"}}
+        {"activeId":14,"items":{"item-0":{"id":0,"type":"Dialog","parentId":false,"style":{"enabled":true,"varName":"KeyFrameRepeaterWindow","windowType":"Palette","creationProps":{"su1PanelCoordinates":true,"maximizeButton":false,"minimizeButton":false,"independent":false,"closeButton":true,"borderless":false,"resizeable":true},"text":"Rotation Repeater","preferredSize":[300,0],"margins":16,"orientation":"column","spacing":10,"alignChildren":["fill","center"]}},"item-1":{"id":1,"type":"StaticText","parentId":5,"style":{"enabled":true,"varName":null,"creationProps":{"truncate":"none","multiline":false,"scrolling":false},"softWrap":false,"text":"Key 1: ","justify":"left","preferredSize":[0,0],"alignment":"fill","helpTip":null}},"item-2":{"id":2,"type":"EditText","parentId":5,"style":{"enabled":true,"varName":"key1Val","creationProps":{"noecho":false,"readonly":false,"multiline":false,"scrollable":false,"borderless":false,"enterKeySignalsOnChange":false},"softWrap":false,"text":"-5","justify":"left","preferredSize":[50,0],"alignment":"fill","helpTip":"第一关键帧数值"}},"item-3":{"id":3,"type":"Panel","parentId":0,"style":{"enabled":true,"varName":"RotationRepeate","creationProps":{"borderStyle":"etched","su1PanelCoordinates":true},"text":"Rotation Repeat （重复旋转关键帧）","preferredSize":[0,0],"margins":10,"orientation":"row","spacing":40,"alignChildren":["left","top"],"alignment":null}},"item-5":{"id":5,"type":"Group","parentId":3,"style":{"enabled":true,"varName":"key1","preferredSize":[0,0],"margins":0,"orientation":"row","spacing":10,"alignChildren":["left","center"],"alignment":null}},"item-10":{"id":10,"type":"Group","parentId":3,"style":{"enabled":true,"varName":"key2","preferredSize":[0,0],"margins":0,"orientation":"row","spacing":10,"alignChildren":["left","center"],"alignment":null}},"item-11":{"id":11,"type":"StaticText","parentId":10,"style":{"enabled":true,"varName":null,"creationProps":{"truncate":"none","multiline":false,"scrolling":false},"softWrap":false,"text":"Key 2: ","justify":"left","preferredSize":[0,0],"alignment":"fill","helpTip":null}},"item-12":{"id":12,"type":"EditText","parentId":10,"style":{"enabled":true,"varName":"key2Val","creationProps":{"noecho":false,"readonly":false,"multiline":false,"scrollable":false,"borderless":false,"enterKeySignalsOnChange":false},"softWrap":false,"text":"5","justify":"left","preferredSize":[50,0],"alignment":"fill","helpTip":"中间关键帧数值"}},"item-14":{"id":14,"type":"Button","parentId":0,"style":{"enabled":true,"varName":"applyBttn","text":"Apply | 应用","justify":"center","preferredSize":[0,0],"alignment":"center","helpTip":null}},"item-15":{"id":15,"type":"Group","parentId":3,"style":{"enabled":true,"varName":"freq","preferredSize":[0,0],"margins":0,"orientation":"row","spacing":10,"alignChildren":["center","center"],"alignment":null}},"item-16":{"id":16,"type":"StaticText","parentId":15,"style":{"enabled":true,"varName":null,"creationProps":{"truncate":"none","multiline":false,"scrolling":false},"softWrap":false,"text":"Frequency: ","justify":"left","preferredSize":[0,0],"alignment":"fill","helpTip":"频率（按秒数计算）"}},"item-17":{"id":17,"type":"EditText","parentId":15,"style":{"enabled":true,"varName":"freq","creationProps":{"noecho":false,"readonly":false,"multiline":false,"scrollable":false,"borderless":false,"enterKeySignalsOnChange":false},"softWrap":false,"text":"0.1","justify":"left","preferredSize":[50,0],"alignment":"fill","helpTip":null}},"item-18":{"id":18,"type":"Checkbox","parentId":3,"style":{"enabled":true,"varName":"holdFrame","text":"Hold Keyframe","preferredSize":[0,0],"alignment":"center","helpTip":null}}},"order":[0,3,5,1,2,10,11,12,15,16,17,18,14],"settings":{"importJSON":true,"indentSize":false,"cepExport":false,"includeCSSJS":true,"showDialog":false,"functionWrapper":false,"afterEffectsDockable":true,"itemReferenceList":"var"}}
         */ 
 
-        // ROTATIONREPEATERWINDOW
+        // KEYFRAMEREPEATERWINDOW
         // ======================
-        var RotationRepeaterWindow = (panelGlobal instanceof Panel) ? panelGlobal : new Window("palette", undefined, undefined, {su1PanelCoordinates: true, resizeable: true}); 
-            if ( !(panelGlobal instanceof Panel) ) RotationRepeaterWindow.text = "Rotation Repeater";  
-            RotationRepeaterWindow.orientation = "column"; 
-            RotationRepeaterWindow.alignChildren = ["Left","center"]; 
-            RotationRepeaterWindow.spacing = 10; 
-            RotationRepeaterWindow.margins = 16; 
+        var keyFrameRepeaterWindow = (panelGlobal instanceof Panel) ? panelGlobal : new Window("palette", undefined, undefined, {su1PanelCoordinates: true, resizeable: true}); 
+            if ( !(panelGlobal instanceof Panel) ) keyFrameRepeaterWindow.text = "KeyFrame Repeater";  
+            keyFrameRepeaterWindow.orientation = "column"; 
+            keyFrameRepeaterWindow.alignChildren = ["Left","center"]; 
+            keyFrameRepeaterWindow.spacing = 10; 
+            keyFrameRepeaterWindow.margins = 16; 
 
         // KEY1
         // ====
-        var key1 = RotationRepeaterWindow.add("group", undefined, {name: "key1"}); 
+        var key1 = keyFrameRepeaterWindow.add("group", undefined, {name: "key1"}); 
             key1.orientation = "row"; 
             key1.alignChildren = ["left","center"]; 
             key1.spacing = 10; 
@@ -42,7 +37,7 @@
 
         // KEY2
         // ====
-        var key2 = RotationRepeaterWindow.add("group", undefined, {name: "key2"}); 
+        var key2 = keyFrameRepeaterWindow.add("group", undefined, {name: "key2"}); 
             key2.orientation = "row"; 
             key2.alignChildren = ["left","center"]; 
             key2.spacing = 10; 
@@ -61,7 +56,7 @@
 
         // FREQ
         // ====
-        var freq = RotationRepeaterWindow.add("group", undefined, {name: "freq"}); 
+        var freq = keyFrameRepeaterWindow.add("group", undefined, {name: "freq"}); 
             freq.orientation = "row"; 
             freq.alignChildren = ["center","center"]; 
             freq.spacing = 10; 
@@ -79,23 +74,23 @@
 
         // ROTATIONREPEATE
         // ===============
-        var holdFrame = RotationRepeaterWindow.add("checkbox", undefined, undefined, {name: "holdFrame"}); 
+        var holdFrame = keyFrameRepeaterWindow.add("checkbox", undefined, undefined, {name: "holdFrame"}); 
             holdFrame.text = "Hold Keyframe"; 
             holdFrame.alignment = ["left","center"]; 
 
         // ROTATIONREPEATERWINDOW
         // ======================
-        var applyBttn = RotationRepeaterWindow.add("button", undefined, undefined, {name: "applyBttn"}); 
+        var applyBttn = keyFrameRepeaterWindow.add("button", undefined, undefined, {name: "applyBttn"}); 
             applyBttn.text = "Apply | 应用"; 
             applyBttn.alignment = ["center","center"]; 
 
-        RotationRepeaterWindow.layout.layout(true);
-        RotationRepeaterWindow.layout.resize();
-        RotationRepeaterWindow.onResizing = RotationRepeaterWindow.onResize = function () { this.layout.resize(); }
+        keyFrameRepeaterWindow.layout.layout(true);
+        keyFrameRepeaterWindow.layout.resize();
+        keyFrameRepeaterWindow.onResizing = keyFrameRepeaterWindow.onResize = function () { this.layout.resize(); }
 
         // ITEM REFERENCE LIST ( Info: http://jongware.mit.edu/Sui/index_1.html ) 
-        RotationRepeaterWindow.items = { 
-        RotationRepeaterWindow: RotationRepeaterWindow, // palette
+        keyFrameRepeaterWindow.items = { 
+        RotationRepeaterWindow: keyFrameRepeaterWindow, // palette
         key1: key1, // group
         key1Val: key1Val, // edittext
         key2: key2, // group
@@ -105,7 +100,7 @@
         holdFrame: holdFrame, // checkbox
         applyBttn: applyBttn // button
         };
-        RotationRepeaterWindow.itemsArray = [RotationRepeaterWindow, RotationRepeaterWindow, key1, key1Val, key2, key2Val, freq, freq1, holdFrame, applyBttn];
+        keyFrameRepeaterWindow.itemsArray = [keyFrameRepeaterWindow, keyFrameRepeaterWindow, key1, key1Val, key2, key2Val, freq, freq1, holdFrame, applyBttn];
 
         var val1 = Number(key1Val.text);
         var val2 = Number(key2Val.text);
@@ -117,15 +112,13 @@
 
         //click button triger event
         applyBttn.onClick = function(){
-            rotationRepe(val1, val2, frequency, holdFrame.value);
+            keyFrameRepe(val1, val2, frequency, holdFrame.value);
         }
 
-        if ( RotationRepeaterWindow instanceof Window ) RotationRepeaterWindow.show();
-        //return RotationRepeaterWindow;
-    //}
+        if ( keyFrameRepeaterWindow instanceof Window ) keyFrameRepeaterWindow.show();
     
-    //Rotation Repeater function
-    function rotationRepe(value1, value2, frequency, keyStatusIsHold){
+    //Key Frame Repeater function
+    function keyFrameRepe(value1, value2, frequency, keyStatusIsHold){
         app.beginUndoGroup("Rotation Repeater");
         
         var selected = app.project.activeItem.selectedProperties; //获得所有选择的属性
@@ -149,27 +142,73 @@
             return
         }
 
-        //支持多选properties
-        for(var i=0; i < selected.length; i++){
-
-            //alert("i = " + i + "\nlength = " + selected.length + "\nTime: " + time);
-
-            var key1 = selected[i].addKey(time);
-            selected[i].setValueAtKey(key1, value1);
-
-            var key2 = selected[i].addKey(time+timeFrequency);
-            selected[i].setValueAtKey(key2, value2);
-
-            var key3 = selected[i].addKey(time+timeFrequency*2);
-            selected[i].setValueAtKey(key3, value1);
-
-            if(keyStatusIsHold == true){
-                selected[i].setInterpolationTypeAtKey(key1, KeyframeInterpolationType.HOLD);
-                selected[i].setInterpolationTypeAtKey(key2, KeyframeInterpolationType.HOLD);
-                selected[i].setInterpolationTypeAtKey(key3, KeyframeInterpolationType.HOLD);
+        //删除旧keyframe
+        try {
+            for (numkey = 1, numkey <= selected.numKeys(), numkey+=1) {
+                selected.removeKey(numkey);
             }
+        }catch(err){
+            alert(err);
+        }
 
-            selected[i].expression = 'loopOut(type = "cycle", numKeyframes = 0)';
+        try {
+            //一维数值的属性操作
+            for(var i=0; i < selected.length; i++){
+
+                //alert("i = " + i + "\nlength = " + selected.length + "\nTime: " + time);
+
+                var key1 = selected[i].addKey(time);
+                selected[i].setValueAtKey(key1, value1);
+
+                var key2 = selected[i].addKey(time+timeFrequency);
+                selected[i].setValueAtKey(key2, value2);
+
+                var key3 = selected[i].addKey(time+timeFrequency*2);
+                selected[i].setValueAtKey(key3, value1);
+
+                //调整 Easy Ease 状态
+                if(keyStatusIsHold == true){
+                    selected[i].setInterpolationTypeAtKey(key1, KeyframeInterpolationType.HOLD);
+                    selected[i].setInterpolationTypeAtKey(key2, KeyframeInterpolationType.HOLD);
+                    selected[i].setInterpolationTypeAtKey(key3, KeyframeInterpolationType.HOLD);
+                }else if(keyStatusIsHold == false){
+                    selected[i].setInterpolationTypeAtKey(key1, KeyframeInterpolationType.LINEAR);
+                    selected[i].setInterpolationTypeAtKey(key2, KeyframeInterpolationType.LINEAR);
+                    selected[i].setInterpolationTypeAtKey(key3, KeyframeInterpolationType.LINEAR);
+                }
+
+                selected[i].expression = 'loopOut(type = "cycle", numKeyframes = 0)';
+            }
+        }catch(err){
+            //二维数组的属性操作
+            for(var i=0; i < selected.length; i++){
+
+                //alert("i = " + i + "\nlength = " + selected.length + "\nTime: " + time);
+
+                var key1 = selected[i].addKey(time);
+                selected[i].setValueAtKey(key1, [value1, value1]);
+
+                var key2 = selected[i].addKey(time+timeFrequency);
+                selected[i].setValueAtKey(key2, [value2, value2]);
+
+                var key3 = selected[i].addKey(time+timeFrequency*2);
+                selected[i].setValueAtKey(key3, [value1, value1]);
+
+                //调整 Easy Ease 状态
+                if(keyStatusIsHold == true){
+                    selected[i].setInterpolationTypeAtKey(key1, KeyframeInterpolationType.HOLD);
+                    selected[i].setInterpolationTypeAtKey(key2, KeyframeInterpolationType.HOLD);
+                    selected[i].setInterpolationTypeAtKey(key3, KeyframeInterpolationType.HOLD);
+                }else if(keyStatusIsHold == false){
+                    selected[i].setInterpolationTypeAtKey(key1, KeyframeInterpolationType.LINEAR);
+                    selected[i].setInterpolationTypeAtKey(key2, KeyframeInterpolationType.LINEAR);
+                    selected[i].setInterpolationTypeAtKey(key3, KeyframeInterpolationType.LINEAR);
+                }
+
+                selected[i].expression = 'loopOut(type = "cycle", numKeyframes = 0)';
+            }
+        }finally{
+            alert(err);
         }
 
         app.endUndoGroup();
